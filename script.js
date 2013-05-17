a69165a1917219912f60fc0aa3cdbd1ea7701592
Wes Kocher <kwierso@gmail.com>
2013-05-17 18:01:36 -0500

    INITIAL

---
+++ script.js
@@ -0,0 +1,28 @@
+var bdayinput = document.getElementsByTagName("input")[0];
+var button = document.getElementsByTagName("input")[1];
+
+button.addEventListener("click", calculateTotal);
+
+function calculateTotal() {
+  let number = 0;
+  let value = bdayinput.value;
+  let date = new Date(value);
+  let day = date.getDate().toString();
+  let year = date.getFullYear().toString();
+  let month = (date.getMonth() + 1).toString();
+  
+  for(i in day) {
+    console.log(day[i]);
+    number = number + parseInt(day[i]);
+  }
+  for(i in month) {
+    console.log(month[i]);
+    number = number + parseInt(month[i]);
+  }
+  for(i in year) {
+    console.log(year[i]);
+    number = number + parseInt(year[i]);
+  }
+  
+  console.log(number);
+}
